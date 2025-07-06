28a94ef6f3fd61d32cabf30ee92d994433b8611d
JohnnyDat06 <hotandat4321@gmail.com>
2025-07-06 15:00:52 +0700

    Update Animation

--- Assets/Data/Scripts/Player/PlayerController.cs
+++ Assets/Data/Scripts/Player/PlayerController.cs
@@ -6,15 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D playerRigidbody;
     [SerializeField] private Animator playerAnimator;
+    [SerializeField] private BoxCollider2D playerCollider;
+    [SerializeField] private LayerMask layerGroud;
 
     [Header("Movement Settings")]
-    private float movementInputDerection;
-    private bool isFacingRight = true;
-    private bool isWalking;
-
     [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private float jumpForce = 16f;
 
+    private float movementInputDerection;
+    private bool isFacingRight = true;
+
+    private enum MovementState { Idle, Walk, JumpStart, JumpEnd}
     void Start()
     {
         
@@ -24,7 +26,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         CheckInput();
-        CheckMovementDirection();
         UpdateAnimation();
     }
 
@@ -33,37 +34,36 @@ public class PlayerController : MonoBehaviour
         ApplyMovement();
     }
 
-    private void CheckMovementDirection()
-    {
-        if (isFacingRight && movementInputDerection < 0 )
-        {
-            Flip();
-        }
-        else if (!isFacingRight && movementInputDerection > 0)
-        {
-            Flip();
-        }
-
-        if(playerRigidbody.velocity.x != 0)
-        {
-            isWalking = true;
-        }
-        else
-        {
-            isWalking = false;
-        }
-    }
 
     private void UpdateAnimation()
     {
-        playerAnimator.SetBool("isWalking", isWalking);
+        MovementState state;
+        // Move
+        if (movementInputDerection > 0)
+        {
+            state = MovementState.Walk;
+            if(!isFacingRight) Flip();
+        }
+        else if (movementInputDerection < 0)
+        {
+            state = MovementState.Walk;
+            if(isFacingRight) Flip();
+        }
+        else state = MovementState.Idle;
+
+        // Jump
+        if (playerRigidbody.velocity.y > 0.1f) state = MovementState.JumpStart;
+        else if (playerRigidbody.velocity.y < -0.1f) state = MovementState.JumpEnd;
+
+        playerAnimator.SetInteger("State", (int)state);
     }
 
     private void CheckInput()
     {
         movementInputDerection = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetButtonDown("Jump") && IsGrounded() == true)
         {
             Jump();
         }
@@ -71,12 +71,26 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+        if (IsGrounded() == true)
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+        }           
     }
 
     private void ApplyMovement()
     {
         playerRigidbody.velocity = new Vector2(movementInputDerection * movementSpeed, playerRigidbody.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(playerCollider.bounds.center, 
+            playerCollider.bounds.size, 
+            0f, 
+            Vector2.down, 
+            0.1f, 
+            layerGroud
+        );
     }
 
     private void Flip()
