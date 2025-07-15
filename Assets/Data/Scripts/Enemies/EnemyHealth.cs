92728af5338e1d03f93597310763a07bb220ce48
JohnnyDat06 <hotandat4321@gmail.com>
2025-07-15 22:42:02 +0700

    Add Enemy1 animations, controller, and health script
    Introduced animation assets and controller for Enemy1, including Attack, Death, Hit, Idle, Revive, and Walk animations, as well as associated sprites. Added EnemyHealth.cs script for enemy health management. Updated Player.unity scene and TagManager settings to integrate the new enemy.

---
+++ Assets/Data/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private float startingHealth = 3f;
+
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private Transform player;
+
+    private bool isDead;
+    private bool preventRespawn = false;
+    private bool isPlayerNearby = false;
+
+    protected virtual void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    protected virtual void Update()
+    {
+        if (isDead && isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            preventRespawn = true;
+        }
+        
+        CheckPlayer();
+    }
+    public virtual void TakeDamage(float damage)
+    {
+        if (isDead) return;
+
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0f, startingHealth);
+        Debug.Log(currentHealth);
+        if (currentHealth > 0)
+        {
+            if (anim != null) anim.SetTrigger("IsHit");
+        }
+        else
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        if (isDead) return;
+
+        isDead = true;
+        Debug.Log("Enemy chết rồi!");
+
+        if (anim != null) anim.SetBool("IsDeath", isDead);
+
+        StartCoroutine(WaitForKillOrRespawn(5f));
+    }
+
+    IEnumerator WaitForKillOrRespawn(float delay)
+    {
+        float timer = 0f;
+
+        // Trong vòng delay giây, chờ người chơi nhấn E
+        while (timer < delay)
+        {
+            if (preventRespawn)
+            {
+                Debug.Log("Đã bị khóa còng");
+                Destroy(gameObject, 10f); // Xóa quái vật
+                yield break; // Thoát coroutine, không hồi sinh nữa
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // Nếu hết thời gian mà không bị kết liễu thì hồi sinh
+        currentHealth = Mathf.RoundToInt(startingHealth * 0.3f);
+        isDead = false;
+        Debug.Log("Enemy hồi sinh với máu: " + currentHealth);
+
+        if (anim != null) anim.SetBool("IsDeath", isDead);
+    }
+
+
+    void CheckPlayer()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        if (distanceToPlayer < 1f && isDead)
+        {
+            isPlayerNearby = true;
+        }
+        else
+        {
+            isPlayerNearby = false;
+        }
+    }
+}
--- Assets/Data/Scripts/Player/ComboAttack.cs
+++ Assets/Data/Scripts/Player/ComboAttack.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.Timeline;
 
 public class ComboAttack : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     [SerializeField] private PlayerController playerController;
+    private EnemyHealth enemyHealth;
+
+    [SerializeField] private LayerMask attackMask;
+    [SerializeField] private Vector2 attackOffset;
+
     private int noOfClicks = 0;
     private float lastClikedTime = 0f;
     [HideInInspector] public bool isAttacking;
+    [Header("Attack Setting")]
+    [SerializeField] private int atk1 = 20;
+    [SerializeField] private int atk2 = 30;
+    [SerializeField] private int atk3 = 50;
+    [SerializeField] private float attackRange = 10f;
     [SerializeField] private float maxComboDelay = 0.9f;
 
 
@@ -29,7 +41,10 @@ public class ComboAttack : MonoBehaviour
         }
     }
 
-
+    private void Awake()
+    {
+        enemyHealth = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyHealth>();
+    }
 
     public void Return1()
     {
@@ -73,13 +88,53 @@ public class ComboAttack : MonoBehaviour
     public void Atk1()
     {
         if(playerController != null) playerController.AttackMoveForward(0.2f, 0.1f);
+
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null && enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(atk1);
+        }
     }
     public void Atk2()
     {
         if (playerController != null) playerController.AttackMoveForward(0.4f, 0.1f);
+
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null && enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(atk2);
+        }
     }
     public void Atk3()
     {
         if (playerController != null) playerController.AttackMoveForward(0.6f, 0.1f);
+
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null && enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(atk3);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(pos, attackRange);
     }
 }
