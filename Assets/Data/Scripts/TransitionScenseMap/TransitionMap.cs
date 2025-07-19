65a57f8eb34d9aceffe36383ce9687536db2f75e
JohnnyDat06 <hotandat4321@gmail.com>
2025-07-19 23:09:26 +0700

    Add MapError scene transition and update button actions
    Introduced a MapError method in TransitionMap to load the MapError scene. Updated Player.unity to trigger MapError on specific button clicks and added MapError.unity to the build settings.

--- Assets/Data/Scripts/TransitionScenseMap/TransitionMap.cs
+++ Assets/Data/Scripts/TransitionScenseMap/TransitionMap.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TransitionMap : MonoBehaviour
 {
@@ -23,5 +24,10 @@ public class TransitionMap : MonoBehaviour
                 isOpen = true;
             }
         }
+    }
+
+    public void MapError()
+    {
+        SceneManager.LoadScene("MapError");
     }
 }
