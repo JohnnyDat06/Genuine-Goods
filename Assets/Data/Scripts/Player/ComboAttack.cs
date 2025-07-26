cbbacac533fbfb7630928ad93562452cb66dc08e
JohnnyDat06 <hotandat4321@gmail.com>
2025-07-27 00:51:07 +0700

    Refactor combo attack logic and adjust enemy health scaling
    Refactored ComboAttack to consolidate attack logic, reduce code duplication, and improve enemy list management. Added dynamic enemy health scaling in EnemyHealth based on MissionManager fail count. Updated PlayerController to simplify parry state handling and integrate parry with attack movement restrictions.

--- Assets/Data/Scripts/Player/ComboAttack.cs
+++ Assets/Data/Scripts/Player/ComboAttack.cs
@@ -1,29 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TestTools;
-using UnityEngine.Timeline;
 
 public class ComboAttack : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     [SerializeField] private PlayerController playerController;
-    //private EnemyHealth enemyHealth;
-    private List<EnemyHealth> allEnemies = new List<EnemyHealth>();
 
+    private List<EnemyHealth> allEnemies = new List<EnemyHealth>();
     [SerializeField] private LayerMask attackMask;
     [SerializeField] private Vector2 attackOffset;
 
     private int noOfClicks = 0;
     private float lastClikedTime = 0f;
     [HideInInspector] public bool isAttacking;
+
     [Header("Attack Setting")]
     [SerializeField] private int atk1 = 20;
     [SerializeField] private int atk2 = 30;
     [SerializeField] private int atk3 = 50;
     [SerializeField] private float attackRange = 10f;
     [SerializeField] private float maxComboDelay = 0.9f;
-
 
     void Update()
     {
@@ -42,9 +39,10 @@ public class ComboAttack : MonoBehaviour
         }
     }
 
-    private void Awake()
+    private void UpdateEnemyList()
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        allEnemies.Clear();
 
         foreach (GameObject enemy in enemies)
         {
@@ -65,9 +63,7 @@ public class ComboAttack : MonoBehaviour
         }
         else
         {
-            anim.SetBool("Attack1", false);
-            noOfClicks = 0;
-            isAttacking = false;
+            ResetAllAnimations();
         }
     }
 
@@ -80,112 +76,71 @@ public class ComboAttack : MonoBehaviour
         }
         else
         {
-            anim.SetBool("Attack2", false);
-            anim.SetBool("Attack1", false);
-            noOfClicks = 0;
-            isAttacking = false;
+            ResetAllAnimations();
         }
     }
+
     public void Return3()
     {
-         anim.SetBool("Attack1", false);
-         anim.SetBool("Attack2", false);
-         anim.SetBool("Attack3", false);
-         noOfClicks = 0;
-         isAttacking = false;
+        ResetAllAnimations();
+    }
+
+    private void ResetAllAnimations()
+    {
+        anim.SetBool("Attack1", false);
+        anim.SetBool("Attack2", false);
+        anim.SetBool("Attack3", false);
+        noOfClicks = 0;
+        isAttacking = false;
+    }
+
+    private void Attack(int damage)
+    {
+        UpdateEnemyList();
+
+        Vector3 pos = transform.position + transform.right * attackOffset.x + transform.up * attackOffset.y;
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+
+        if (colInfo != null)
+        {
+            foreach (EnemyHealth eh in allEnemies.ToArray())
+            {
+                if (eh == null)
+                {
+                    allEnemies.Remove(eh);
+                    continue;
+                }
+
+                if (colInfo.transform.IsChildOf(eh.transform) || colInfo.transform == eh.transform)
+                {
+                    eh.TakeDamage(damage);
+                    break;
+                }
+            }
+        }
     }
 
     public void Atk1()
     {
-        if(playerController != null) playerController.AttackMoveForward(0.2f, 0.1f);
-
-        Vector3 pos = transform.position;
-        pos += transform.right * attackOffset.x;
-        pos += transform.up * attackOffset.y;
-
-        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
-        if (colInfo != null)
-        {
-            foreach (EnemyHealth eh in allEnemies.ToArray())
-            {
-                if (eh == null)
-                {
-                    allEnemies.Remove(eh); // cleanup nếu enemy đã bị destroy
-                    continue;
-                }
-
-                if (colInfo.transform.IsChildOf(eh.transform) || colInfo.transform == eh.transform)
-                {
-                    eh.TakeDamage(atk1);
-                    break;
-                }
-            }
-
-        }
+        if (playerController != null) playerController.AttackMoveForward(0.2f, 0.1f);
+        Attack(atk1);
     }
+
     public void Atk2()
     {
         if (playerController != null) playerController.AttackMoveForward(0.4f, 0.1f);
-
-        Vector3 pos = transform.position;
-        pos += transform.right * attackOffset.x;
-        pos += transform.up * attackOffset.y;
-
-        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
-        if (colInfo != null)
-        {
-            foreach (EnemyHealth eh in allEnemies.ToArray())
-            {
-                if (eh == null)
-                {
-                    allEnemies.Remove(eh); // cleanup nếu enemy đã bị destroy
-                    continue;
-                }
-
-                if (colInfo.transform.IsChildOf(eh.transform) || colInfo.transform == eh.transform)
-                {
-                    eh.TakeDamage(atk2);
-                    break;
-                }
-            }
-
-        }
+        Attack(atk2);
     }
+
     public void Atk3()
     {
         if (playerController != null) playerController.AttackMoveForward(0.6f, 0.1f);
-
-        Vector3 pos = transform.position;
-        pos += transform.right * attackOffset.x;
-        pos += transform.up * attackOffset.y;
-
-        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
-        if (colInfo != null)
-        {
-            foreach (EnemyHealth eh in allEnemies.ToArray())
-            {
-                if (eh == null)
-                {
-                    allEnemies.Remove(eh); // cleanup nếu enemy đã bị destroy
-                    continue;
-                }
-
-                if (colInfo.transform.IsChildOf(eh.transform) || colInfo.transform == eh.transform)
-                {
-                    eh.TakeDamage(atk3);
-                    break;
-                }
-            }
-
-        }
+        Attack(atk3);
     }
 
     private void OnDrawGizmosSelected()
     {
-        Vector3 pos = transform.position;
-        pos += transform.right * attackOffset.x;
-        pos += transform.up * attackOffset.y;
-
+        Vector3 pos = transform.position + transform.right * attackOffset.x + transform.up * attackOffset.y;
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(pos, attackRange);
     }
