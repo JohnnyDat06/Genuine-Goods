7ffb79af7d6ccc5194474191d138e06781191f5e
Hieu1904c <nguyeenshieeus@gmail.com>
2025-08-02 02:37:23 +0700

    xong cua mat khau

---
+++ Assets/Data/Scripts/CauDo/cuaMatKhau/TriggerEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TriggerEvents : MonoBehaviour
+{
+    // Kéo object PasswordDoor vào đây trong Inspector
+    public PasswordDoorController doorController;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorController.OnPlayerEnterTrigger();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorController.OnPlayerExitTrigger();
+        }
+    }
+}
