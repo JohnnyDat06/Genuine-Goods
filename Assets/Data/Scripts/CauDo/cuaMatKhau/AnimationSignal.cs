864738297bc4e1cd87fd1e5ca18872aceeb900ff
Hieu1904c <nguyeenshieeus@gmail.com>
2025-08-02 05:26:29 +0700

    fix

---
+++ Assets/Data/Scripts/CauDo/cuaMatKhau/AnimationSignal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events; // Quan trọng: Phải có dòng này
+
+public class AnimationSignal : MonoBehaviour
+{
+    // Tạo ra một sự kiện có thể tùy chỉnh trong Inspector
+    public UnityEvent onAnimationEvent;
+
+    // Hàm này sẽ được gọi bởi Animation Event
+    public void SendSignal()
+    {
+        // Bắn tín hiệu ra cho bất kỳ ai đang lắng nghe
+        onAnimationEvent.Invoke();
+    }
+}
