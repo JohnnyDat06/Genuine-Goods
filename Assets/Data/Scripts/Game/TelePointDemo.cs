fc1fed7cace6d2396f195632359c9ac5c7239278
JohnnyDat06 <hotandat4321@gmail.com>
2025-08-06 00:45:21 +0700

    Refactor enemy assets and update animation settings
    Moved enemy assets from Light/Enemies to Enemies directory and updated animation curves for Enemy 3 and 4 death animations. Adjusted spritePixelsToUnits for Enemy 3 and 4 assets, and added Android platform settings for Enemy 2 texture. Minor prefab and scene updates included.

--- Assets/Data/Scripts/Game/TelePointDemo.cs
+++ Assets/Data/Scripts/Game/TelePointDemo.cs
@@ -7,23 +7,18 @@ using UnityEngine.SceneManagement;
 public class TelePointDemo : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    
+    [SerializeField] private string sceneName;
     [SerializeField] private List<GameObject> points = new List<GameObject>();
 
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F1)) player.transform.position = points[0].transform.position;
         if (Input.GetKeyDown(KeyCode.F2)) player.transform.position = points[1].transform.position;
         if (Input.GetKeyDown(KeyCode.F3)) player.transform.position = points[2].transform.position;
-        if (Input.GetKeyDown(KeyCode.F4)) SceneManager.LoadScene("Map2");
-        if (Input.GetKeyDown(KeyCode.F5)) player.transform.position = points[3].transform.position;
-        if (Input.GetKeyDown(KeyCode.F6)) SceneManager.LoadScene("MiniBoss1");
-        if (Input.GetKeyDown(KeyCode.F7)) SceneManager.LoadScene("FinalBoss");
-        if (Input.GetKeyDown(KeyCode.F8)) SceneManager.LoadScene("Map1");
+        if (Input.GetKeyDown(KeyCode.F4)) player.transform.position = points[3].transform.position;
+        if (Input.GetKeyDown(KeyCode.F5)) player.transform.position = points[4].transform.position;
+        if (Input.GetKeyDown(KeyCode.F6)) player.transform.position = points[5].transform.position;
+        if (Input.GetKeyDown(KeyCode.F7) && sceneName != null) SceneManager.LoadScene(sceneName);
     }
 }
