701b894d751554a612a454cdddf8e866c8529be8
Hieu1904c <nguyeenshieeus@gmail.com>
2025-08-09 10:53:35 +0700

    sua ong tho dien

---
+++ Assets/Data/Scripts/ongThoDien/ElectricianAI.cs
@@ -0,0 +1,18 @@
+﻿// ElectricianAI.cs
+using UnityEngine;
+
+public class ElectricianAI : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        // Đảm bảo Animator Controller của ông thợ điện có 1 parameter dạng bool tên là "IsScared"
+        // và state "Scared" được set để loop.
+        if (anim != null)
+        {
+            anim.SetBool("IsScared", true);
+        }
+    }
+}
